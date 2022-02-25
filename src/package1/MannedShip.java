e9f1445a84fa1cef8a2b4eb20a97ecbf610dac17
Yeiner Hidalgo Molina <yeinerhidalgomolina@gmail.com>
2022-02-25 14:56:50 -0500

    Estructura del método abstracto

--- src/package1/MannedShip.java
+++ src/package1/MannedShip.java
@@ -3,10 +3,28 @@
  */
 package package1;
 
-/**
- *
- * @author yeine
- */
-public class MannedShip {
+
+public class MannedShip extends SpaceShip{
+
+    public MannedShip(String name, int power, String fuel, double weight, float speed, String country, int capacity) {
+        super(name, power, fuel, weight, speed, country, capacity);
+    }
+    
+    
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public String type() {
+        return "";
+    }
+
+    @Override
+    public String destination() {
+        return "";
+    }
     
 }
--- src/package1/UnmannedShip.java
+++ src/package1/UnmannedShip.java
@@ -3,10 +3,27 @@
  */
 package package1;
 
-/**
- *
- * @author yeine
- */
-public class UnmannedShip {
+public class UnmannedShip extends SpaceShip{
+
+    public UnmannedShip(String name, int power, String fuel, double weight, float speed, String country, int capacity) {
+        super(name, power, fuel, weight, speed, country, capacity);
+    }
+    
+    
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public String type() {
+        return "";
+    }
+
+    @Override
+    public String destination() {
+        return "";
+    }
     
 }
